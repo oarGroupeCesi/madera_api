9bb91a016820c2221e5b849cf55f1bca08b44951
Romain Richard <romain.richard.19@gmail.com>
2016-02-06 02:22:12 +0100

    Add CRUD Customer

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Auth;
+use Validator;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth.basic.once');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Customer::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'lastname' => 'required',
+            'firstname' => 'required',
+            'email' => 'required|unique:customers',
+            'adr_street' => 'required',
+            'adr_zipcode' => 'required',
+            'adr_city' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return [
+                'errors' => $validator->errors()->all()
+            ];
+        }
+
+        $customer = Customer::create($request->all());
+        
+        return $customer;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return [
+                'errors' => [
+                    'User does not exist.',
+                ],
+            ];
+        }
+        return $customer;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return [
+                'errors' => [
+                    'User does not exist.',
+                ],
+            ];
+        }
+        $customer->update($request->all());
+        return $customer;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customer = Customer::find($id);
+
+        if (!$customer) {
+            return [
+                'errors' => [
+                    'User does not exist.',
+                ],
+            ];
+        }
+        $customer->delete();
+        return 'User has been delete';
+    }
+}
---
+++ app/Models/Customer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'customers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'firstname',
+    	'lastname',
+    	'email',
+    	'adr_street',
+    	'adr_zipcode',
+    	'adr_city',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+}
