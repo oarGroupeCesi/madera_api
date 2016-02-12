1663b9c48b931d10e08c418672c78ea87fb094ea
Romain Richard <romain.richard.19@gmail.com>
2016-02-12 14:22:54 +0100

    Add cast for date fields in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
@@ -22,5 +29,15 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
     ];
 }
