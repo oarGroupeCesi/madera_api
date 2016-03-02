c76b1e3da0b64a40009ee1d27a3ea7c99165a952
alexisAvenel <avenel.alexis@sfr.fr>
2016-03-02 21:47:31 +0100

    Mise à jour du Cors (problème d'accès)

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,11 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin' , '*')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+        return $next($request)
+          ->header('Access-Control-Allow-Origin', 'http://madera-local.dev')
+          ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+          ->header('Access-Control-Allow-Headers',  'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
+          ->header('Access-Control-Allow-Credentials', 'true')
+          ->header('Access-Control-Max-Age', '1728000');
     }
 }
