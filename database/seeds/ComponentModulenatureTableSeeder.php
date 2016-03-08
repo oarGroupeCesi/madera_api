d39aef199740b62f694c73c66f76372d7fe8dc96
Romain Richard <romain.richard.19@gmail.com>
2016-03-08 19:37:17 +0100

    Add new seeder for component module nature link

--- database/seeds/ComponentModulenatureTableSeeder.php
+++ database/seeds/ComponentModulenatureTableSeeder.php
@@ -20,6 +20,49 @@ class ComponentModulenatureTableSeeder extends Seeder
 	        ]);
     	}
 
+        // Cloison intérieure
+        for ($i=1; $i < 6; $i++) { 
+            DB::table('component_modulenature')->insert([
+                'quantity' => 13,
+                'component_id' => $i,
+                'modulenature_id' => 2,
+            ]);
+        }
 
+        // Plancher sur dalle
+        for ($i=1; $i < 8; $i++) { 
+            DB::table('component_modulenature')->insert([
+                'quantity' => 24,
+                'component_id' => $i,
+                'modulenature_id' => 3,
+            ]);
+        }
+
+        // Plancher porteur
+        for ($i=1; $i < 4; $i++) { 
+            DB::table('component_modulenature')->insert([
+                'quantity' => 35,
+                'component_id' => $i,
+                'modulenature_id' => 4,
+            ]);
+        }
+
+        // Ferme de charpente
+        for ($i=1; $i < 10; $i++) { 
+            DB::table('component_modulenature')->insert([
+                'quantity' => 50,
+                'component_id' => $i,
+                'modulenature_id' => 5,
+            ]);
+        }
+
+        // Couverture
+        for ($i=1; $i < 13; $i++) { 
+            DB::table('component_modulenature')->insert([
+                'quantity' => 100,
+                'component_id' => $i,
+                'modulenature_id' => 6,
+            ]);
+        }
     }
 }
