6597052120d25375968f830a62ad887b2aa8ff84
Romain Richard <romain.richard.19@gmail.com>
2016-03-08 17:57:42 +0100

    Add test to user

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,21 +1,23 @@
 <?php
 
-// use Illuminate\Foundation\Testing\WithoutMiddleware;
-// use Illuminate\Foundation\Testing\DatabaseMigrations;
-// use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-// class UserTest extends TestCase
-// {
+class UserTest extends TestCase
+{
 
-//     /**
-//      * A basic test example.
-//      *
-//      * @return void
-//      */
-//     public function testAuthentication()
-//     {
-//     	$response = $this->call('POST', '/api/auth/login', ['email' => 'administrateur@madera.local', 'password' => 'admin']);
+    use WithoutMiddleware;
 
-//     	$this->assertEquals(200, $response->status());
-//     }
-// }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAuthentication()
+    {
+    	$response = $this->call('POST', '/api/auth/login', ['email' => 'administrateur@madera.local', 'password' => 'admin']);
+
+    	$this->assertEquals(200, $response->status());
+    }
+}
