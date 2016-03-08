21905a21dc60c1868b472355b287286d0af331c4
Romain Richard <romain.richard.19@gmail.com>
2016-03-08 17:27:22 +0100

    Add test for auth to API

---
+++ tests/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAuthentication()
+    {
+    	$response = $this->call('POST', '/api/auth/login', ['email' => 'administrateur@madera.local', 'password' => 'admin']);
+
+    	$this->assertEquals(200, $response->status());
+    }
+}
