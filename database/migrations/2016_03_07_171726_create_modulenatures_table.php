c15df5ea97ca19b1d4a1f7446ca3af174d0cac5e
Romain Richard <romain.richard.19@gmail.com>
2016-03-08 19:27:33 +0100

    Add price for module nature

--- database/migrations/2016_03_07_171726_create_modulenatures_table.php
+++ database/migrations/2016_03_07_171726_create_modulenatures_table.php
@@ -16,6 +16,7 @@ class CreateModulenaturesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('unity');
+            $table->integer('price');
             $table->timestamps();
         });
     }
--- database/seeds/ModulenaturesTableSeeder.php
+++ database/seeds/ModulenaturesTableSeeder.php
@@ -13,43 +13,49 @@ class ModulenaturesTableSeeder extends Seeder
     public function run()
     {
         DB::table('modulenatures')->insert([
-            'name' => 'Mûr extérieur',
-            'unity' => 'M linéaire',
+            'name'       => 'Mûr extérieur',
+            'unity'      => 'M linéaire',
+            'price'      => 2000,
             'created_at' => Carbon::now(Config::get('app.timezone')),
             'updated_at' => Carbon::now(Config::get('app.timezone')),
         ]);
 
         DB::table('modulenatures')->insert([
-            'name' => 'Cloison intérieure',
-            'unity' => 'M linéaire',
+            'name'       => 'Cloison intérieure',
+            'unity'      => 'M linéaire',
+            'price'      => 2300,
             'created_at' => Carbon::now(Config::get('app.timezone')),
             'updated_at' => Carbon::now(Config::get('app.timezone')),
         ]);
 
         DB::table('modulenatures')->insert([
-            'name' => 'Plancher sur dalle',
-            'unity' => 'M2',
+            'name'       => 'Plancher sur dalle',
+            'unity'      => 'M2',
+            'price'      => 3000,
             'created_at' => Carbon::now(Config::get('app.timezone')),
             'updated_at' => Carbon::now(Config::get('app.timezone')),
         ]);
 
         DB::table('modulenatures')->insert([
-            'name' => 'Plancher porteur',
-            'unity' => 'M2',
+            'name'       => 'Plancher porteur',
+            'unity'      => 'M2',
+            'price'      => 1900,
             'created_at' => Carbon::now(Config::get('app.timezone')),
             'updated_at' => Carbon::now(Config::get('app.timezone')),
         ]);
 
         DB::table('modulenatures')->insert([
-            'name' => 'Ferme de charpente',
-            'unity' => 'Unité',
+            'name'       => 'Ferme de charpente',
+            'unity'      => 'Unité',
+            'price'      => 2500,
             'created_at' => Carbon::now(Config::get('app.timezone')),
             'updated_at' => Carbon::now(Config::get('app.timezone')),
         ]);
 
         DB::table('modulenatures')->insert([
-            'name' => 'Couverture',
-            'unity' => 'M2',
+            'name'       => 'Couverture',
+            'unity'      => 'M2',
+            'price'      => 5000,
             'created_at' => Carbon::now(Config::get('app.timezone')),
             'updated_at' => Carbon::now(Config::get('app.timezone')),
         ]);
