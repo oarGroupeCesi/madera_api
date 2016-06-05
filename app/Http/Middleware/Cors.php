e52d1f41cb660370c66671d1b192306de352130a
alexisAvenel <avenel.alexis@sfr.fr>
2016-06-05 22:57:40 +0200

    modification url cors front

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-          ->header('Access-Control-Allow-Origin', 'http://madera-local.dev')
+          ->header('Access-Control-Allow-Origin', 'http://local.dev.madera')
           ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
           ->header('Access-Control-Allow-Headers',  'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
           ->header('Access-Control-Allow-Credentials', 'true')
